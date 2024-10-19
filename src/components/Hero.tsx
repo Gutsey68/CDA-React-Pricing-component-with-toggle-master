b5cae846b55ae51a7329488d5cf8dec8396cd9ed
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-10-19 10:52:57 +0200

    Update toggle order in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,9 @@ function Hero() {
         <section className="text-center flex flex-col items-center">
             <h1 className="text-4xl font-bold tracking-wide text-slate-500 mb-10">Our Pricing</h1>
             <div className="flex items-center text-slate-400 font-medium gap-5">
-                <p>Annually</p>
-                <Toggle />
                 <p>Monthly</p>
+                <Toggle />
+                <p>Annually</p>
             </div>
         </section>
     );
